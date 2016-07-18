679a3e4cdd8aba250dfb18300bf52f0dee9a3b98
Peter Seller <Peter.Seller@logmein.com>
2016-07-18 10:44:54 +0200

    Add test for Hello

---
+++ test_hello.py
@@ -0,0 +1,20 @@
+import hello
+import unittest
+
+
+class HelloTest(unittest.TestCase):
+
+    def setUp(self):
+        hello.app.config['TESTING'] = True
+        self.app = hello.app.test_client()
+
+    def tearDown(self):
+        pass
+
+    def test_hello(self):
+        rv = self.app.get('/')
+        assert "Hello, World!" in rv.data
+
+
+if __name__ == '__main__':
+    unittest.main()
