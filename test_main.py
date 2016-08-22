192e9cb6f3d410c60031c80e7e116e89097f76e3
Peter Seller <Peter.Seller@logmein.com>
2016-08-22 16:46:57 +0200

    Add create new movie test.

--- test_main.py
+++ test_main.py
@@ -1,3 +1,4 @@
+from flask import json
 import unittest
 
 import main
@@ -5,11 +6,13 @@ import main
 
 class MainTest(unittest.TestCase):
     def setUp(self):
+        self.a_movie_data = {"title": "Interstellar", "year": 2014, "director": "Christopher Nolan"}
+
         main.app.config['TESTING'] = True
         self.app = main.app.test_client()
 
     def tearDown(self):
-        pass
+        self.app.application.movies.__init__();
 
     def test_hello(self):
         rv = self.app.get('/')
@@ -18,6 +21,12 @@ class MainTest(unittest.TestCase):
     def test_get_movie_nonexisting(self):
         response = self.app.get('/movies/1')
         assert response.status_code == 404
+
+    def test_create_new_movie(self):
+        response = self.app.post('/movies/'
+                                 , data=json.dumps(self.a_movie_data)
+                                 , content_type='application/json')
+        assert response.status_code == 200
 
 
 if __name__ == '__main__':
