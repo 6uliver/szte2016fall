f240247be24911e04daa7268d21b17479392d031
Peter Seller <Peter.Seller@logmein.com>
2016-08-23 18:12:49 +0200

    Add mock based tests

--- test_main.py
+++ test_main.py
@@ -1,8 +1,11 @@
 import unittest
 
+from assertpy import assert_that
 from flask import json
+from mock import Mock
 
 import main
+from model.movies import Movies
 
 
 class MainTest(unittest.TestCase):
@@ -13,7 +16,7 @@ class MainTest(unittest.TestCase):
         self.app = main.app.test_client()
 
     def tearDown(self):
-        self.app.application.movies.__init__();
+        self.app.application.movies = Movies()
 
     def test_hello(self):
         rv = self.app.get('/')
@@ -33,11 +36,34 @@ class MainTest(unittest.TestCase):
         assert response.status_code == 200
         assert json_data['title'] == "Interstellar"
 
+    def test_get_movie_existing_without_post(self):
+        self.app.application.movies.movies[1] = self.a_movie_data
+        response = self.app.get('/movies/1')
+
+        assert_that(response.status_code).is_equal_to(200)
+
+    def test_get_movie_existing_with_mock(self):
+        self.app.application.movies = Mock()
+        self.app.application.movies.get_movie = Mock(return_value=self.a_movie_data)
+        response = self.app.get('/movies/1')
+
+        assert_that(response.status_code).is_equal_to(200)
+
     def test_create_new_movie(self):
         response = self.app.post('/movies/'
                                  , data=json.dumps(self.a_movie_data)
                                  , content_type='application/json')
         assert response.status_code == 200
+
+    def test_create_new_movie_with_mock(self):
+        self.app.application.movies = Mock()
+        self.app.application.movies.create_movie = Mock(return_value=self.a_movie_data)
+
+        self.app.post('/movies/'
+                      , data=json.dumps(self.a_movie_data)
+                      , content_type='application/json')
+
+        self.app.application.movies.create_movie.assert_called_once_with(self.a_movie_data)
 
 
 if __name__ == '__main__':
