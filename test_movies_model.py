c03821a84a7caa746f3ad346a4c92777defddce1
Peter Seller <Peter.Seller@logmein.com>
2016-08-23 13:59:20 +0200

    Rename test variables in MoviesModelTest

--- test_movies_model.py
+++ test_movies_model.py
@@ -7,33 +7,33 @@ from model.movies import Movies
 
 class MoviesModelTest(unittest.TestCase):
     def setUp(self):
-        self.aMovieData = {}
-        self.otherMovieData = {}
-        self.model = Movies()
+        self.a_movie_data = {}
+        self.other_movie_data = {}
+        self.movie_model = Movies()
 
     def tearDown(self):
         pass
 
     def test_get_movie_nonexisting(self):
-        result = self.model.get_movie(1)
+        result = self.movie_model.get_movie(1)
 
         assert_that(result).is_false()
 
     def test_get_movie_existing(self):
-        self.model.create_movie(self.aMovieData)
-        result = self.model.get_movie(1)
+        self.movie_model.create_movie(self.a_movie_data)
+        result = self.movie_model.get_movie(1)
 
         assert_that(result).contains_key('id')
 
     def test_create_movie_different_ids(self):
-        a_movie = self.model.create_movie(self.aMovieData)
-        other_movie = self.model.create_movie(self.otherMovieData)
+        a_movie = self.movie_model.create_movie(self.a_movie_data)
+        other_movie = self.movie_model.create_movie(self.other_movie_data)
 
         assert_that(a_movie['id']).is_not_equal_to(other_movie['id'])
 
     def test_create_movie_does_not_alter_data(self):
         moviedata = {}
-        self.model.create_movie(moviedata)
+        self.movie_model.create_movie(moviedata)
 
         assert_that(moviedata).does_not_contain_key('id')
 
